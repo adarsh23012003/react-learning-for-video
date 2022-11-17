e34f9c4715a23e1983971dfed6ef38e54318930b
Adarsh Maurya <adarshmaurya2303@gmail.com>
2022-11-17 15:42:55 +0530

    Functional Component is added

--- src/Components/ComponentInfo.jsx
+++ src/Components/ComponentInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import FunctionalComponent from './FunctionalComponent'
 
 function ComponentInfo() {
   return (
@@ -6,6 +7,7 @@ function ComponentInfo() {
     <h1>ComponentInfo :-</h1>
     <h3>Component are two type </h3>
     <h5>1. Functional Component</h5>
+    <FunctionalComponent/>
     <h5>2. Class Component</h5>
     </>
   )
---
+++ src/Components/FunctionalComponent.jsx
@@ -0,0 +1,14 @@
+// This is a Functional Component
+
+import React from 'react'
+
+// FunctionalComponent ==> rfce
+function FunctionalComponent() {
+  return (
+    <>
+      <p>This is a Functional Component</p>
+    </>
+  );
+}
+
+export default FunctionalComponent
