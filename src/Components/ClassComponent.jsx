9fc1587369182f54a9841a25eace3a13ae4fa466
Adarsh Maurya <adarshmaurya2303@gmail.com>
2022-11-17 15:44:13 +0530

    ClassComponent is added

---
+++ src/Components/ClassComponent.jsx
@@ -0,0 +1,14 @@
+// This is a Class Component
+
+import React, { Component } from 'react'
+
+// ClassComponent ==> rcc
+// ClassComponent ==> Your Component name
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <div>This is a Class Component</div>
+    )
+  }
+}
--- src/Components/ComponentInfo.jsx
+++ src/Components/ComponentInfo.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import FunctionalComponent from './FunctionalComponent'
+import ClassComponent from './ClassComponent'
 
 function ComponentInfo() {
   return (
     <>
-    <h1>ComponentInfo :-</h1>
-    <h3>Component are two type </h3>
-    <h5>1. Functional Component</h5>
-    <FunctionalComponent/>
-    <h5>2. Class Component</h5>
+      <h1>ComponentInfo :-</h1>
+      <h3>Component are two type </h3>
+      <h5>1. Functional Component</h5>
+      <FunctionalComponent />
+      <h5>2. Class Component</h5>
+      <ClassComponent />
     </>
-  )
+  );
 }
 
 export default ComponentInfo
