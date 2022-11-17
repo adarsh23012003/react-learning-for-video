a0df946233edb7ef039c4594563450b368bcc3ba
Adarsh Maurya <adarshmaurya2303@gmail.com>
2022-11-17 14:59:28 +0530

    Component Info add (information)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Introduction from "./Components/Introduction";
+import ComponentInfo from "./Components/ComponentInfo";
 function App() {
   return (
     <div className="App">
     <Introduction />
     <hr />
+    <ComponentInfo/>
     </div>
   );
 }
---
+++ src/Components/ComponentInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ComponentInfo() {
+  return (
+    <>
+    <h1>ComponentInfo :-</h1>
+    <h3>Component are two type </h3>
+    <h5>1. Functional Component</h5>
+    <h5>2. Class Component</h5>
+    </>
+  )
+}
+
+export default ComponentInfo
